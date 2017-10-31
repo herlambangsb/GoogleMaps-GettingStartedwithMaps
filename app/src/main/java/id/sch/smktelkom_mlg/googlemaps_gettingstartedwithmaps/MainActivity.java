07101721714d5944a7112345a9e6885783cbcbfd
HerlambangSB <kuraninja72@gmail.com>
2017-10-31 21:13:00 +0700

    Initial Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/googlemaps_gettingstartedwithmaps/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.googlemaps_gettingstartedwithmaps;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
